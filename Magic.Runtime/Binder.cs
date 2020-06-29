e559bdd1f42878bb80f52385a3011deb7895a3f9
Ramsey Nasser <ram@nas.sr>
2020-06-29 17:14:07 -0400

    Rename runtime subproject to Magic.Runtime

---
+++ Magic.Runtime/Binder.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Linq;
+using System.Globalization;
+using System.Reflection;
+
+namespace Magic
+{
+    public class Binder : System.Reflection.Binder
+    {
+        public static readonly Binder Shared = new Binder();
+
+        System.Reflection.Binder _binder = Type.DefaultBinder;
+
+        public override FieldInfo BindToField(BindingFlags bindingAttr, FieldInfo[] match, object value, CultureInfo culture)
+        {
+            return _binder.BindToField(bindingAttr, match, value, culture);
+        }
+
+#if CSHARP8
+        public override MethodBase? BindToMethod(BindingFlags bindingAttr, MethodBase[] match, ref object[] args, ParameterModifier[]? modifiers, CultureInfo? culture, string[]? names, out object state)
+#else
+        public override MethodBase BindToMethod(BindingFlags bindingAttr, MethodBase[] match, ref object[] args, ParameterModifier[] modifiers, CultureInfo culture, string[] names, out object state)
+#endif
+        {
+            try {
+                var nativeResult = _binder.BindToMethod(bindingAttr, match, ref args, modifiers, culture, names, out state);
+                if(nativeResult != null)
+                    return nativeResult;
+            } catch(MissingMemberException) {
+                // ignore
+            }
+            var argumentTypes = args.Select(a => a == null ? typeof(Object) : a.GetType()).ToArray();
+            state = new Object(); // ???
+            var result = SelectMethod(bindingAttr, match, argumentTypes, modifiers);
+            if(result != null)
+            {
+                var parameters = result.GetParameters();
+                for (int i = 0; i < args.Length; i++)
+                {
+                    // this is assumed to work at this point. a situation where
+                    // we could not convert the argument types should not have
+                    // bound and SelectMethod should have returned null
+                    if(parameters[i].ParameterType.IsEnum) {
+                        args[i] = Enum.ToObject(parameters[i].ParameterType, args[i]);
+
+                    } else if(args[i] is IConvertible) {
+                        args[i] = Convert.ChangeType(args[i], parameters[i].ParameterType);
+
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public override object ChangeType(object value, Type type, CultureInfo culture)
+        {
+            return _binder.ChangeType(value, type, culture);
+        }
+
+        public override void ReorderArgumentArray(ref object[] args, object state)
+        {
+            _binder.ReorderArgumentArray(ref args, state);
+        }
+
+#if CSHARP8        
+        public override MethodBase? SelectMethod(BindingFlags bindingAttr, MethodBase[] match, Type[] argumentTypes, ParameterModifier[]? modifiers)
+#else
+        public override MethodBase SelectMethod(BindingFlags bindingAttr, MethodBase[] match, Type[] argumentTypes, ParameterModifier[] modifiers)
+#endif
+        {
+            if(match.Length == 0)
+                return null;
+
+            for (int i = 0; i < match.Length; i++)
+                if(match[i] == null)
+                    return null;
+
+            for (int i = 0; i < argumentTypes.Length; i++)
+                if(argumentTypes[i] == null)
+                    return null;
+            
+#if CSHARP8
+            MethodBase? result = null;
+#else
+            MethodBase result = null;
+#endif
+            // SelectMethod on the default binder throws an exception if passed
+            // typebuilders. this comes up when trying to bind to proxy methods
+            // during analysis when (the proxy type is not yet complete). we
+            // skip the default binder in this case and fall back on our own logic
+            bool allRuntimeTypes = true;
+            for (var i=0; i<argumentTypes.Length; i++) {
+                if (argumentTypes[i] is System.Reflection.Emit.TypeBuilder) {
+                    allRuntimeTypes = false;
+                    break;
+                }
+            }
+            if(allRuntimeTypes)
+                result = _binder.SelectMethod(bindingAttr, match, argumentTypes, modifiers);
+            if (result != null)
+                return result;
+            foreach (var candidate in match)
+            {
+                if (MatchByMagicBindingRules(bindingAttr, candidate, argumentTypes, modifiers))
+                {
+                    if (result != null)
+                        throw new AmbiguousMatchException();
+                    else
+                    {
+                        result = candidate;
+                        continue;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static bool MatchesByEnumConversionConversion(Type argumentType, Type parameterType)
+            => parameterType.IsEnum
+               && parameterType.GetEnumUnderlyingType() == argumentType;
+
+        static bool MatchesByObjectConversion(Type argumentType, Type parameterType)
+            => argumentType == typeof(object)
+               || parameterType.IsAssignableFrom(argumentType);
+
+        static bool MatchesByNarrowingConversion(Type argumentType, Type parameterType)
+            => parameterType == argumentType
+               || (parameterType.IsPrimitive && argumentType.IsPrimitive && parameterType != typeof(Boolean) && argumentType != typeof(Boolean))
+               || parameterType.IsAssignableFrom(argumentType);
+        
+#if CSHARP8
+        static bool MatchByMagicBindingRules(BindingFlags bindingAttr, MethodBase candidate, Type[] argumentTypes, ParameterModifier[]? modifiers)
+#else
+        static bool MatchByMagicBindingRules(BindingFlags bindingAttr, MethodBase candidate, Type[] argumentTypes, ParameterModifier[] modifiers)
+#endif
+        {
+            var parameters = candidate.GetParameters();
+            if (parameters.Length != argumentTypes.Length) return false;
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var parameterType = parameters[i].ParameterType;
+                var argumentType = argumentTypes[i];
+                if(MatchesByNarrowingConversion(argumentType, parameterType)
+                   || MatchesByEnumConversionConversion(argumentType, parameterType)
+                   || MatchesByObjectConversion(argumentType, parameterType))
+                {
+                    continue;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override PropertyInfo SelectProperty(BindingFlags bindingAttr, PropertyInfo[] match, Type returnType, Type[] indexes, ParameterModifier[] modifiers)
+        {
+            return _binder.SelectProperty(bindingAttr, match, returnType, indexes, modifiers);
+        }
+    }
+}
---
+++ Magic.Runtime/Runtime.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Reflection;
+using System.Linq;
+
+namespace Magic
+{
+    public static class Runtime
+    {
+        public static void InvokeInitType(Assembly assy, Type initType)
+        {
+            try
+            {
+                initType.InvokeMember("Initialize", BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.Public, Type.DefaultBinder, null, new object[0]);
+            }
+            catch (Exception e)
+            {
+                throw new TypeLoadException(String.Format("Error initializing {0}: {1}", assy.FullName, e.Message), e);
+            }
+        }
+
+        public static string ClojureCLRInitClassName(string sourcePath)
+        {
+            return "__Init__$" + sourcePath.Replace(".", "/").Replace("/", "$");
+        }
+
+        public static bool TryLoadInitType(string relativePath)
+        {
+            var initClassName = ClojureCLRInitClassName(relativePath);
+            Type initType = null;
+            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (asm.IsDynamic)
+                    continue;
+                initType = asm.GetType(initClassName);
+                if (initType != null)
+                    break;
+            }
+            if (initType == null)
+                return false;
+
+            InvokeInitType(initType.Assembly, initType);
+            return true;
+        }
+
+        // exists to conform with IL2CPP assumptions about a clean stack after a throw
+        public static void ThrowHelper(Exception e)
+        {
+            throw e;
+        }
+
+#if CSHARP8
+        public static Type? FindType(string p)
+#else
+        public static Type FindType(string p)
+#endif
+        {
+#if CSHARP8
+            Type? t = null;
+#else
+            Type t = null;
+#endif
+            // fastest path, will succeed for assembly qualified names (returned by Type.AssemblyQualifiedName)
+            // or namespace qualified names (returned by Type.FullName) in the executing assembly or mscorlib
+            // e.g. "UnityEngine.Transform, UnityEngine, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null"
+            t = Type.GetType(p, false);
+
+            if (t != null)
+                return t;
+
+            AppDomain domain = AppDomain.CurrentDomain;
+            Assembly[] assys = domain.GetAssemblies();
+
+            // fast path, will succeed for namespace qualified names (returned by Type.FullName)
+            // e.g. "UnityEngine.Transform"
+            foreach (Assembly assy in assys)
+            {
+                  Type t1 = assy.GetType(p, false);
+                  if(t1 != null)
+                        return t1;
+            }
+
+            // TODO parse name for complex types
+            // if (t == null && p.IndexOfAny(_triggerTypeChars) != -1)
+            //     t = ClrTypeSpec.GetTypeFromName(p);
+
+            return t;
+        }
+    }
+    public static class Dispatch
+    {
+
+#if CSHARP8
+        static object InvokeUnwrappingExceptions(MethodBase method, object? target, object[]? args)
+#else
+        static object InvokeUnwrappingExceptions(MethodBase method, object target, object[]? args)
+#endif
+        {
+            try {
+                return method.Invoke(target, args);
+            } catch (TargetInvocationException e) {
+                throw e.InnerException;
+            }
+        }
+
+        // (set! (.name o) value)
+        public static object SetMember(object o, string name, object value)
+        {
+            var oType = o.GetType();
+            var valueType = value.GetType();
+            var field = oType.GetField(name);
+            if (field != null)
+            {
+                if(field.FieldType.IsPrimitive && valueType.IsPrimitive)
+                    value = Convert.ChangeType(value, field.FieldType);
+                field.SetValue(o, value);
+                return value;
+            }
+            var property = oType.GetProperty(name);
+            if (property != null)
+            {
+                if(property.PropertyType.IsPrimitive && valueType.IsPrimitive)
+                    value = Convert.ChangeType(value, property.PropertyType);
+                property.SetValue(o, value);
+                return value;
+            }
+            throw new Exception($"Could not set member `{name}` on target {o.ToString()}, no such member exists.");
+        }
+        
+        // (.name o)
+        public static object InvokeZeroArityMember(object o, string name)
+        {
+            var oType = o == null ? typeof(Object) : o.GetType();
+            var field = oType.GetField(name);
+            if (field != null)
+                return field.GetValue(o);
+            var property = oType.GetProperty(name);
+            if (property != null)
+                return InvokeUnwrappingExceptions(property.GetMethod, o, null);
+            var method = oType.GetMethod(name, Type.EmptyTypes);
+            if (method != null)
+                return InvokeUnwrappingExceptions(method, o, null);
+            throw new Exception($"Could not invoke zero arity member `{name}` on target {(o == null ? "null" : o.ToString())}.");
+        }
+
+        static MethodBase? BindToMethod(BindingFlags bindingFlags, Type t, string name, object[] args)
+        {
+            var methods = t.GetMethods().Where(m => m.Name == name).ToArray();
+            Object state;
+#if CSHARP8
+            MethodBase? method;
+#else
+            MethodBase method;
+#endif
+            method = null;
+            if(methods.Length > 0)
+                 method = Binder.Shared.BindToMethod(bindingFlags,methods,ref args,null,null,null,out state);
+            return method;            
+        }
+
+        public static object InvokeInstanceMethod(object o, string name, object[] args)
+        {
+            var method = BindToMethod(BindingFlags.Public | BindingFlags.Instance, o.GetType(), name, args);
+            if(method != null)
+                return InvokeUnwrappingExceptions(method, o, args);
+            throw new Exception($"Could not invoke instance member method `{name}` on target {o.ToString()} ({o.GetType()}) with argument types { String.Join<Object>(", ", args) }.");
+        }
+
+        public static object InvokeStaticMethod(object t, string name, object[] args)
+        {
+            var method = BindToMethod(BindingFlags.Public | BindingFlags.Static, (Type)t, name, args);
+            if(method != null)
+                return InvokeUnwrappingExceptions(method, null, args);
+            throw new Exception($"Could not invoke static member method `{name}` on target {t} with argument types { String.Join<Object>(", ", args) }.");
+        }
+
+        public static object InvokeConstructor(Type t, object[] args)
+        {
+            return Activator.CreateInstance(t, args);
+        }
+    }
+}
